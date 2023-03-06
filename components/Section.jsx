a56b72bdbc649c05f91aa2c01d471636d9716d67
unknown <darioguerraaragon@gmail.com>
2023-03-06 18:59:30 -0500

    preparado

--- components/Section.jsx
+++ components/Section.jsx
@@ -3,24 +3,12 @@ import MoviesCards from './MoviesCards'
 
 const Section = ({data}) => { 
   console.log(data)
-
-  let box = []
-
-  if(data == undefined){
-    null
-  }else{
-    data.forEach(el => {
-        box.push(1)
-    });
-  }
-
-  console.log(box)
-  
+  data = [1 ,2 ]
   return (
     <section>
       <article>
         {data == undefined?null:data.map((el)=>(<MoviesCards key={el.id}/>))}
-        {box.map((el)=>(<MoviesCards/>))}
+        {/* {box.map((el)=>(<MoviesCards/>))} */}
       </article>     
     </section>
   )
